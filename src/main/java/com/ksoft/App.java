34b3ce70c8121cc94b0940aff8d229b34e6831cb
kamlendra <kamlendra.chauhan@gmail.com>
2018-11-09 12:35:18 +0530

    first

---
+++ src/main/java/com/ksoft/App.java
@@ -0,0 +1,13 @@
+package com.ksoft;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
